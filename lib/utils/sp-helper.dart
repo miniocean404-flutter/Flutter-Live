55f1fc4ab8a74059b68e574fe8a6371eb9128df8
戴海洋 <1037306928@qq.com>
2021-10-29 18:49:00 +0800

    添加sphelper工具类

--- lib/utils/sp-helper.dart
+++ lib/utils/sp-helper.dart
@@ -1,175 +1,104 @@
-// import 'dart:convert';
-// import 'dart:async';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:synchronized/synchronized.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class SpHelper {
-//   static SpHelper _intance;
-//   static SharedPreferences _prefs;
-//   static Lock _lock = Lock();
+class SpHelper {
+  // 静态变量_instance，存储唯一对象
+  static late SpHelper _instance;
+  late SharedPreferences prefs;
 
-//   static Future<SpHelper> getInstance() async {
-//     if (_intance == null) {
-//       await _lock.synchronized(() async {
-//         if (_intance == null) {
-//           // 保持本地实例直到完全初始化。
-//           var instance = SpHelper._(); 
-//           await instance._init();
-//           _intance = instance;
-//         }
-//       });
-//     }
-//     return _intance;
-//   }
+  // instance的getter方法，SpHelper.instance获取对象
+  static SpHelper get instance => _getInstance();
 
-//   // 私有构造函数
-//   SpHelper._();
+  // 工厂方法构造函数
+  factory SpHelper() => _getInstance();
 
-//   Future _init() async {
-//     _prefs = await SharedPreferences.getInstance();
-//   }
+  // 获取对象
+  static SpHelper _getInstance() {
+    _instance = SpHelper._internal();
+    return _instance;
+  }
 
-//   /// put object.
-//   static Future<bool> putObject(String key, Object value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setString(key, value == null ? "" : json.encode(value));
-//   }
+  SpHelper._internal() {
+    SharedPreferences.getInstance().then((value) => prefs = value);
+  }
 
-//   /// get obj.
-//   static T getObj<T>(String key, T f(Map v), {T defValue}) {
-//     Map map = getObject(key);
-//     return map == null ? defValue : f(map);
-//   }
+  // 存数据
+  static Object savePreference(
+    BuildContext context,
+    String key,
+    Object value,
+  ) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (value is int) {
+      await prefs.setInt(key, value);
+    } else if (value is double) {
+      await prefs.setDouble(key, value);
+    } else if (value is bool) {
+      await prefs.setBool(key, value);
+    } else if (value is String) {
+      await prefs.setString(key, value);
+    } else if (value is List<String>) {
+      await prefs.setStringList(key, value);
+    } else {
+      throw new Exception("不能得到这种类型");
+    }
+  }
 
-//   /// get object.
-//   static Map getObject(String key) {
-//     if (_prefs == null) return null;
-//     String _data = _prefs.getString(key);
-//     return (_data == null || _data.isEmpty) ? null : json.decode(_data);
-//   }
+  // 取数据
+  static Future getPreference(
+    Object context,
+    String key,
+    Object defaultValue,
+  ) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (defaultValue is int) {
+      return prefs.getInt(key);
+    } else if (defaultValue is double) {
+      return prefs.getDouble(key);
+    } else if (defaultValue is bool) {
+      return prefs.getBool(key);
+    } else if (defaultValue is String) {
+      return prefs.getString(key);
+    } else if (defaultValue is List) {
+      return prefs.getStringList(key);
+    } else {
+      throw new Exception("不能得到这种类型");
+    }
+  }
 
-//   /// put object list.
-//   static Future<bool> putObjectList(String key, List<Object> list) {
-//     if (_prefs == null) return null;
-//     List<String> _dataList = list?.map((value) {
-//       return json.encode(value);
-//     })?.toList();
-//     return _prefs.setStringList(key, _dataList);
-//   }
+  // 获取任意值的key
+  static void getAny(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.get(key);
+  }
 
-//   /// get obj list.
-//   static List<T> getObjList<T>(String key, T f(Map v),
-//       {List<T> defValue = const []}) {
-//     List<Map> dataList = getObjectList(key);
-//     List<T> list = dataList?.map((value) {
-//       return f(value);
-//     })?.toList();
-//     return list ?? defValue;
-//   }
+  // 获取所有值
+  static void getKeys() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getKeys();
+  }
 
-//   /// get object list.
-//   static List<Map> getObjectList(String key) {
-//     if (_prefs == null) return null;
-//     List<String> dataLis = _prefs.getStringList(key);
-//     return dataLis?.map((value) {
-//       Map _dataMap = json.decode(value);
-//       return _dataMap;
-//     })?.toList();
-//   }
+  // 如果持久化数据中存在给定的key则返回true
+  static void containsKey(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.containsKey(key); //删除指定键
+  }
 
-//   /// get string.
-//   static String getString(String key, {String defValue = ''}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.getString(key) ?? defValue;
-//   }
+  // 删除指定数据
+  static void remove(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(key); //删除指定键
+  }
 
-//   /// put string.
-//   static Future<bool> putString(String key, String value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setString(key, value);
-//   }
+  // 清空整个缓存
+  static void clear() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
 
-//   /// get bool.
-//   static bool getBool(String key, {bool defValue = false}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.getBool(key) ?? defValue;
-//   }
-
-//   /// put bool.
-//   static Future<bool> putBool(String key, bool value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setBool(key, value);
-//   }
-
-//   /// get int.
-//   static int getInt(String key, {int defValue = 0}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.getInt(key) ?? defValue;
-//   }
-
-//   /// put int.
-//   static Future<bool> putInt(String key, int value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setInt(key, value);
-//   }
-
-//   /// get double.
-//   static double getDouble(String key, {double defValue = 0.0}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.getDouble(key) ?? defValue;
-//   }
-
-//   /// put double.
-//   static Future<bool> putDouble(String key, double value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setDouble(key, value);
-//   }
-
-//   /// get string list.
-//   static List<String> getStringList(String key,
-//       {List<String> defValue = const []}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.getStringList(key) ?? defValue;
-//   }
-
-//   /// put string list.
-//   static Future<bool> putStringList(String key, List<String> value) {
-//     if (_prefs == null) return null;
-//     return _prefs.setStringList(key, value);
-//   }
-
-//   /// get dynamic.
-//   static dynamic getDynamic(String key, {Object defValue}) {
-//     if (_prefs == null) return defValue;
-//     return _prefs.get(key) ?? defValue;
-//   }
-
-//   /// have key.
-//   static bool haveKey(String key) {
-//     if (_prefs == null) return null;
-//     return _prefs.getKeys().contains(key);
-//   }
-
-//   /// get keys.
-//   static Set<String> getKeys() {
-//     if (_prefs == null) return null;
-//     return _prefs.getKeys();
-//   }
-
-//   /// remove.
-//   static Future<bool> remove(String key) {
-//     if (_prefs == null) return null;
-//     return _prefs.remove(key);
-//   }
-
-//   /// clear.
-//   static Future<bool> clear() {
-//     if (_prefs == null) return null;
-//     return _prefs.clear();
-//   }
-
-//   ///Sp is initialized.
-//   static bool isInitialized() {
-//     return _prefs != null;
-//   }
-// }
+  // 重新加载缓存的share
+  static void reload() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.reload();
+  }
+}
