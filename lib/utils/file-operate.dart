74a2b89dde2d26841decf16a2d7074c67576beae
MiniOcean404 <1037306928@qq.com>
2022-03-27 21:07:08 +0800

    添加注释

--- lib/utils/file-operate.dart
+++ lib/utils/file-operate.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:my_app/utils/util.dart';
 
+// 图片保存插件
 saveAssetsGallery({String assets = '', String target = ''}) async {
   ByteData bytes = await rootBundle.load(assets);
 
